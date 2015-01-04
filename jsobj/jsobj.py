c02ad0c97a0a0ca96e2512f31fe668f8caf26c76
Geza Kovacs <gkovacs@stanford.edu>
2015-01-04 00:08:48 -0800

    fix issue when object is in array

--- jsobj/jsobj.py
+++ jsobj/jsobj.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# md5: dfc71d017ee785325e27afd62b615699
+# md5: 673eca544291f6b5a6106dfb7a2d30e4
 # coding: utf-8
 
 """
@@ -11,14 +11,24 @@ Copyright (c) 2014, Geza Kovacs. Based on JsObject by Marcin Wierzbanowski.
 License: MIT (see LICENSE for details)
 """
 #from __future__ import with_statement
-#import os
-#HERE = os.path.abspath(os.path.dirname(__file__))
+import os
+try:
+  HERE = os.path.abspath(os.path.dirname(__file__))
+except:
+  HERE = os.path.abspath(os.getcwd())
 
 __author__ = 'Geza Kovacs'
-#__version__ = open(os.path.join(HERE, '..', 'VERSION')).read()[:-1]
-__version__ = '1.0.0'
+__version__ = open(os.path.join(HERE, '..', 'VERSION')).read()[:-1]
+#__version__ = '1.0.0'
 __license__ = 'MIT'
 
+
+def to_jsobj(v):
+  if type(v) == dict:
+    return Object(v)
+  if type(v) == list:
+    return [to_jsobj(x) for x in v]
+  return v
 
 class Object(object):
   """ This is a base class """
@@ -44,16 +54,20 @@ class Object(object):
       return dict((k, self.__get(v)) for (k, v) in self.__dict__.items())
 
   def __get(self, v):
-    return v.get() if type(v) == Object else v
+    #return v.get() if type(v) == Object else v
+    return v
 
   def __set(self, v):
-    return Object(v) if type(v) == dict else v
+    #return Object(v) if type(v) == dict else v
+    return to_jsobj(v)
 
   def __str__(self):
-    return str(self.get())
+    #return str(self.get())
+    return self.__dict__.__str__()
 
   def __repr__(self):
-    return str(self.get())
+    #return str(self.get())
+    return self.__dict__.__repr__()
 
   def __eq__(self, other):
     return str(self) == str(other)
@@ -71,7 +85,8 @@ class Object(object):
       return self.__dict__[k]
 
   def __setitem__(self, k, v):
-    self.__dict__[k] = Object(v) if type(v) == dict else v
+    #self.__dict__[k] = Object(v) if type(v) == dict else v
+    self.__dict__[k] = to_jsobj(v)
 
   def __iter__(self):
     return self.__dict__.__iter__()
